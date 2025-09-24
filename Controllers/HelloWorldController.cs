16ca79ac542a3ca3ab30ecdefb47a5ef006d04cb
kimihito <tatsurotamashiro@gmail.com>
2025-09-24 14:53:20 +0900

    クエリパラメータ

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -7,14 +7,14 @@ public class HelloWorldController : Controller
 {
   //
   // GET: /HelloWorld/
-  public static Index()
+  public string Index()
   {
     return "This is my default action...";
   }
   //
   // GET: /HelloWorld/Welcome/
-  public static Welcome()
+  public string Welcome(string name, int numTimes = 1)
   {
-    return "This is the Welcome action method...";
+    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
   }
 }
